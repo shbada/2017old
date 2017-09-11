c27a5bfcd29b705f62d516cf10fbe6ee576db6fe
Youngcheol Bae <soohoin12@naver.com>
2017-09-11 17:07:01 +0900

    (배영철) sts에서 push 시도!!

--- flowerShop/src/main/java/com/flowershop/controller/main/controller/MainController.java
+++ flowerShop/src/main/java/com/flowershop/controller/main/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
+	// 이부분 수정해서 넣어봤어요!! 
 	@RequestMapping("/main")
 	public String mainPage(){
 		return "main/main";

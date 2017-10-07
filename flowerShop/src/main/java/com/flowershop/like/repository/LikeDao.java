fcbdc17f2b0765e99f56ba9fdecbd8ad95beb700
seohae <kimseohae96@naver.com>
2017-10-07 21:50:49 +0900

    10.07/삭제

--- flowerShop/src/main/java/com/flowershop/like/repository/LikeDao.java
+++
@@ -1,9 +0,0 @@
-package com.flowershop.like.repository;
-
-public interface LikeDao {
-	/*int LikeSelect(int productNo);
-	
-	ProductVo LikeUpSelect(ProductVo productVo);
-	
-	void likeUpSave(ProductVo productVo);*/
-}
